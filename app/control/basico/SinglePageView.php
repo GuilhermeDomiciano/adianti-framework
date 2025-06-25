b7c14826061390ca661991d1db65f89c9d59f996
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-25 15:07:16 -0300

    Pagina simples

---
+++ app/control/basico/SinglePageView.php
@@ -0,0 +1,27 @@
+<?php 
+
+use Adianti\Widget\Util\TXMLBreadCrumb;
+
+class SinglePageView extends TPage{
+    public function __construct(){
+        parent::__construct(); 
+
+        try{
+            $html = new THtmlRenderer('app\resources\page.html');
+            $replaces = [];
+            $replaces['title'] = 'Título';
+            $replaces['body'] = 'Conteúdo';
+            $replaces['footer'] = 'Rodapé';
+            $html->enableSection('main', $replaces);
+
+            $vbox = new TVBox;
+            $vbox->style='width:100%';
+            $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+            $vbox->add($html);
+
+            parent::add($vbox);
+        }catch (Exception $e){
+            new TMessage('error', $e->getMessage());
+        }
+    }
+}
