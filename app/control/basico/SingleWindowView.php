06ff313abad5c043ad301ae4cfe4bc88164c37b3
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-25 15:43:06 -0300

    Janelas simples

---
+++ app/control/basico/SingleWindowView.php
@@ -0,0 +1,19 @@
+<?php 
+class SingleWindowView extends TWindow{
+    public function __construct(){
+        parent::__construct();
+        parent::setTitle('Título da Janela');
+        parent::setSize(0.5, null);
+        // parent::removePadding();
+        // parent::removeTitleBar();
+
+        $html = new THtmlRenderer('app\resources\page.html');
+        $replaces['title'] = 'Título';
+        $replaces['body'] = 'Conteúdo';
+        $replaces['footer'] = 'Rodapé';
+        $html->enableSection('main', $replaces);
+
+        parent::add($html);
+    }
+
+}
