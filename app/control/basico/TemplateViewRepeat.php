e00addce430d6b25c0f48413679f0b7b8be16f23
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-25 14:42:05 -0300

    Página simples

---
+++ app/control/basico/TemplateViewRepeat.php
@@ -0,0 +1,21 @@
+<?php 
+
+class TemplateViewRepeat extends TPage{
+    public function __construct(){
+        parent::__construct();
+
+        try{
+            $html = new THtmlRenderer('app\resources\template-repeat.html');
+
+            $replace = [];
+            $replace[] = ['id' => 1, 'nome'=> 'Guilherme', 'endereco' => 'Rua 1, 123'];
+            $replace[] = ['id'=> 2, 'nome'=> 'João', 'endereco' => 'Rua 2, 456'];
+            $replace[] = ['id'=> 3, 'nome'=> 'Davi', 'endereco'=> 'Rua 3, 789'];
+            $html->enableSection('main', []);
+            $html->enableSection('details', $replace, true);
+            parent::add($html);
+        }catch (Exception $e){
+            new TMessage('error', $e->getMessage());
+        }
+    }
+}
