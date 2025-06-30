a2a2c94e56f31d3185e877e633664583a35c80ab
Domiciano <guilhermedomicianosilva@gmail.com>
2025-06-30 11:48:01 -0300

    Avançando muito no curso

---
+++ app/control/formularios/FormularioBootstrapEstatico.php
@@ -0,0 +1,41 @@
+<?php
+
+use Adianti\Control\TAction;
+use Adianti\Control\TPage;
+use Adianti\Widget\Form\TEntry;
+use Adianti\Widget\Form\TLabel;
+use Adianti\Widget\Form\TPassword;
+use Adianti\Wrapper\BootstrapFormBuilder;
+
+class FormularioBootstrapEstatico extends TPage
+{
+    private $form;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->form = new BootstrapFormBuilder;
+        $this->form->setFormTitle('Formulário bootstrap estático');
+        
+        $id           = new TEntry('id');
+        $descricao    = new TEntry('descricao');
+        $senha        = new TPassword('senha');
+        
+        $this->form->appendPage('Aba 1');
+        $this->form->addFields( [ new TLabel('Id') ], [$id] );
+        $this->form->addFields( [ new TLabel('Descrição') ], [$descricao] );
+        $this->form->addFields( [ new TLabel('Senha') ], [$senha] );
+        
+        $this->form->addAction( 'Enviar', new TAction( [$this, 'onSend'] ), 'fa:save');
+        
+        parent::add( $this->form );
+    }
+    
+    public static function onSend($param)
+    {
+        echo '<pre>';
+        var_dump($param);
+        echo '</pre>';
+    }
+}
